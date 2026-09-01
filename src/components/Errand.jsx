f45f8158358c91dd5ce14ad8820c727aed7b5263
Peter Migichi <petermigichi47@gmail.com>
2026-09-01 15:45:46 +0300

    separate files

---
+++ src/components/Errand.jsx
@@ -0,0 +1,39 @@
+import { BsArrowRight, BsClock } from "react-icons/bs";
+import { CiLocationOn } from "react-icons/ci";
+
+export function Errand({ name, price, description, destination, time }) {
+  return (
+    <div className="flex flex-col gap-3 mt-5 bg-gray-100 p-4">
+      <div className="flex gap-3">
+        <img
+          src="https://lh3.googleusercontent.com/aida-public/AB6AXuCizjI01qxHM2GVqNDZDZ7CVFFIJQnK2v97h_vT5sYun6ih7K57FhfT6sjEI9Z9oXNpO5jo5pgoB883hLJ0z7wEPYzdKd5OxYDV1LvtlBlwMJC3ISuXturWdVjRov2IqIwrgjE09G2Wjejn8RFNOGko6G33jbFmpKZBGx93i8OiUfyNerR7bnpuAkDL3yyLIjPE272qE585u1YxyzpEVWyOihZh4MbF-uKKfzL51hFpHX4E2ARCkDe6"
+          alt="image"
+          className="w-10 h-10 rounded-full"
+        />
+        <div className="flex flex-col gap-3">
+          <div className="flex justify-between">
+            <h2 className="font-semibold text-xl">{name}</h2>
+            <div className="text-lg text-tertiary font-semibold">
+              <span>ksh.</span>
+              <span>{price}</span>
+            </div>
+          </div>
+          <p className="text-gray-600 text-xs">{description}</p>
+          <div className="flex gap-4 items-center">
+            <div className="flex gap-1">
+              <CiLocationOn className="text-lg" />
+              <span className="text-xs">{destination}</span>
+            </div>
+            <div className="flex gap-1">
+              <BsClock className="text-sm" />
+              <span className="text-xs"> {time} left</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button className="button py-2 cursor-pointer text-white font-semibold text-xl">
+        View Details <BsArrowRight />
+      </button>
+    </div>
+  );
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
 import { BiSearch } from "react-icons/bi";
-import { BsArrowRight, BsClock } from "react-icons/bs";
-import { CiLocationOn } from "react-icons/ci";
 import { FiFilter } from "react-icons/fi";
+import { Errand } from "./Errand";
 
 const errands = [
   {
@@ -10,7 +9,23 @@ const errands = [
     description:
       " Need someone to pick up my clothes from main street. I have already paid for the laundry fee",
     destination: "Katoloni",
-    time: 2,
+    time: "2 hrs",
+  },
+  {
+    name: "Coffee Run",
+    price: 30,
+    description:
+      "Stuck in the library studying for finals. I desperately need a large iced latte from the cafeteria at Kathayoni",
+    destination: "Kathayoni",
+    time: "1 hr",
+  },
+  {
+    name: "Print Notes",
+    price: 50,
+    description:
+      "Printer is broken in my hall. Need 50 pages of notes printed in color and",
+    destination: "Machakos University",
+    time: "8 hrs",
   },
 ];
 
@@ -45,43 +60,6 @@ export function Home() {
           />
         ))}
       </div>
-    </div>
-  );
-}
-
-function Errand({ name, price, description, destination, time }) {
-  return (
-    <div className="flex flex-col gap-3 mt-5 bg-gray-100 p-4">
-      <div className="flex gap-3">
-        <img
-          src="https://lh3.googleusercontent.com/aida-public/AB6AXuCizjI01qxHM2GVqNDZDZ7CVFFIJQnK2v97h_vT5sYun6ih7K57FhfT6sjEI9Z9oXNpO5jo5pgoB883hLJ0z7wEPYzdKd5OxYDV1LvtlBlwMJC3ISuXturWdVjRov2IqIwrgjE09G2Wjejn8RFNOGko6G33jbFmpKZBGx93i8OiUfyNerR7bnpuAkDL3yyLIjPE272qE585u1YxyzpEVWyOihZh4MbF-uKKfzL51hFpHX4E2ARCkDe6"
-          alt="image"
-          className="w-10 h-10 rounded-full"
-        />
-        <div className="flex flex-col gap-3">
-          <div className="flex justify-between">
-            <h2 className="font-semibold text-xl">{name}</h2>
-            <div className="text-lg text-tertiary font-semibold">
-              <span>ksh.</span>
-              <span>{price}</span>
-            </div>
-          </div>
-          <p className="text-gray-600 text-xs">{description}</p>
-          <div className="flex gap-4 items-center">
-            <div className="flex gap-1">
-              <CiLocationOn className="text-lg" />
-              <span className="text-xs">{destination}</span>
-            </div>
-            <div className="flex gap-1">
-              <BsClock className="text-sm" />
-              <span className="text-xs"> {time} left</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <button className="button py-2 cursor-pointer text-white font-semibold text-xl">
-        View Details <BsArrowRight />
-      </button>
     </div>
   );
 }
