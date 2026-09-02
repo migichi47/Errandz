d1c9a571da24c369e5e3273c4201c3b78dd432fe
Peter Migichi <petermigichi47@gmail.com>
2026-09-02 05:58:29 +0300

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import { CiCirclePlus } from "react-icons/ci";
 import { FaRegUser } from "react-icons/fa";
 import { SiBuzzfeed } from "react-icons/si";
+import { useNavigate } from "react-router-dom";
 
 export function Footer() {
+  const navigate = useNavigate();
+
   return (
     <div className="mt-2 absolute w-full bottom-2 flex justify-center gap-10 [&>button]:border [&>button]:border-black/20 [&>button]:rounded-full [&>button]:flex [&>button]:flex-col [&>button]:p-2 [&>button]:w-15 [&>button]:justify-center [&>button]:items-center">
-      <button className="group">
+      <button className="group" onClick={() => navigate("/")}>
         <SiBuzzfeed className="group-hover:" /> <span>Feed</span>
       </button>
       <button>
