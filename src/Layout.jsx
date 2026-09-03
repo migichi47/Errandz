7779a5341708d9358168dd3279685c5f2e319c63
Peter Migichi <petermigichi47@gmail.com>
2026-09-04 00:05:46 +0300

    .

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,7 +6,7 @@ export function Layout() {
   return (
     <div className="relative h-screen">
       <Header />
-      <main className="pt-17 h-screen overflow-scroll px-3 max-w-300 mx-auto">
+      <main className="py-17 h-screen overflow-scroll px-3 max-w-300 mx-auto">
         <Outlet />
       </main>
       <Footer />
